4df5a26cd5529b89c8e83df1962d91c81e14465c
fklska <fklskadelovoy@gmail.com>
2026-03-29 22:41:25 +0300

    session maker

--- backend/app/api/routes/reviews.py
+++ backend/app/api/routes/reviews.py
@@ -1,33 +1,34 @@
+from app.core.db import Session
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/reviews", tags=["reviews"])
 
 
 @router.get("/")
-async def get_reviews():
+async def get_reviews(session: Session):
     pass
 
 
 @router.post("/")
-async def create_review():
+async def create_review(session: Session):
     pass
 
 
 @router.put("/{review_id}")
-async def replace_review(review_id: int):
+async def replace_review(review_id: int, session: Session):
     pass
 
 
 @router.patch("/{review_id}")
-async def edit_review(review_id: int):
+async def edit_review(review_id: int, session: Session):
     pass
 
 
 @router.get("/{review_id}")
-async def get_review(review_id: int):
+async def get_review(review_id: int, session: Session):
     pass
 
 
 @router.delete("/{review_id}")
-async def delete_review(review_id: int):
+async def delete_review(review_id: int, session: Session):
     pass
--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,9 +1,20 @@
+from typing import Annotated
+
 from app.core.config import settings
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 engine = create_async_engine(str(settings.SQLALCHEMY_DATABASE_URI))
-async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
+
+
+Session = Annotated[AsyncSession, Depends(get_async_session)]
 
 
 class Base(DeclarativeBase):
