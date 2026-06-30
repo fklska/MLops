b6c4437ffb7c48035692bf3ba135039debcab481
fklska <fklskadelovoy@gmail.com>
2026-06-30 10:59:37 +0300

    fix

--- monitoring/drift_service.py
+++ monitoring/drift_service.py
@@ -1,7 +1,6 @@
 import os
 
 import pandas as pd
-import psycopg2
 from datasets import load_dataset
 from evidently import Report
 from evidently.presets import (
@@ -9,41 +8,45 @@ from evidently.presets import (
     TextEvals,
 )
 from evidently.ui.workspace import Workspace
+from sqlalchemy import create_engine
 
 DB_HOST = os.getenv("POSTGRES_SERVER", "localhost")
 DB_USER = os.getenv("POSTGRES_USER", "postgres")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 DB_NAME = os.getenv("POSTGRES_DB", "postgres")
 DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-
 WORKSPACE_PATH = "/app/evidently_workspace"
 
-dataset = load_dataset("fklska/bert_sentiment_ds", split="train").shuffle(42).select(range(10000))
-ref = pd.DataFrame(dataset)
-
-conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD, port=DB_PORT)
-query = "SELECT description, label_id FROM review ORDER BY id DESC LIMIT 10000;"
-
-curr = pd.read_sql_query(query, conn)
-conn.close()
+try:
+    engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+    query = "SELECT description, label_id FROM review ORDER BY id DESC LIMIT 10000;"
+    curr = pd.read_sql_query(query, engine)
+except Exception:
+    ws = Workspace.create(WORKSPACE_PATH)
+    project = next((p for p in ws.list_projects() if p.name == "kinootziv_monitoring"), None)
+    if not project:
+        ws.create_project("kinootziv_monitoring")
+    exit(0)
 
 if curr.empty:
     ws = Workspace.create(WORKSPACE_PATH)
-    if not ws.get_project("kinootziv_monitoring"):
+    project = next((p for p in ws.list_projects() if p.name == "kinootziv_monitoring"), None)
+    if not project:
         ws.create_project("kinootziv_monitoring")
     exit(0)
 
 curr.columns = ["text", "label"]
+
+dataset = load_dataset("fklska/bert_sentiment_ds", split="train").shuffle(42).select(range(10000))
+ref = pd.DataFrame(dataset)
 ref = ref[["text", "label"]]
 
-
-drift_report = Report(metrics=[DataDriftPreset(), TextEvals()], include_tests=True)
-
+drift_report = Report(metrics=[DataDriftPreset(), TextEvals()])
 drift_report.run(reference_data=ref, current_data=curr)
 
 ws = Workspace.create(WORKSPACE_PATH)
-project = ws.get_project("kinootziv_monitoring")
+project = next((p for p in ws.list_projects() if p.name == "kinootziv_monitoring"), None)
 if not project:
     project = ws.create_project("kinootziv_monitoring")
 
-ws.add_report(project.id, drift_report)
+ws.add_snapshot(project.id, drift_report)
